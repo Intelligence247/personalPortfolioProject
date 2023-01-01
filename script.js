6c58137453c175f0ff790c5d3849c975928e29eb
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-01-01 08:10:36 +0100

    fixes

--- script.js
+++ script.js
@@ -14,8 +14,4 @@ setInterval(() => {
     secEl.style.rotate = `${sec * 6 - 90}deg`
     minEl.style.rotate = `${min * 6 - 90}deg`
     hourEl.style.rotate = `${(hour * 30 - 90) + min / 2}deg`
-
-
-    // minEl.style.transform = `rotate${min * 6 - 90}deg`
-    console.log(sec / 60 * 360 - 90)
 }, 1000);
