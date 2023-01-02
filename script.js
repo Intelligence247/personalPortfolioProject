0aaa95b7f55460846c0801117ac9e02ec8a69511
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-01-02 12:10:13 +0100

    f

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 const hourEl = document.querySelector('.hour')
 const minEl = document.querySelector('.min')
 const secEl = document.querySelector('.sec')
-
-
+let date = new Date;
+let hour = date.getHours()
+let min = date.getMinutes()
+let sec = date.getSeconds()
+secEl.style.rotate = `${sec * 6 - 90}deg`
+minEl.style.rotate = `${min * 6 - 90}deg`
+hourEl.style.rotate = `${(hour * 30 - 90) + min / 2}deg`
 
 setInterval(() => {
-
     let date = new Date;
     let hour = date.getHours()
     let min = date.getMinutes()
